5f7916e748512a023ce25458e1628151350af4bc
Geice Gomes <sogmesmo@gmail.com>
2021-01-14 16:42:29 -0300

    criando e modificando interface

--- src/background.js
+++ src/background.js
@@ -31,6 +31,12 @@ async function createWindow() {
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
+  win.webContents.on('did-finish-load',  () => {
+    const { title, version } = require('../package.json')
+    win.setTitle(`${title} :: ${version}`)
+ })
+
+
 }
 
 // Quit when all windows are closed.
