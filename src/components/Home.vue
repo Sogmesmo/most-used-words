d43757536c181f7a29ae5eab964d6df08cc6895b
Geice Gomes <sogmesmo@gmail.com>
2021-01-18 16:02:24 -0300

    interface grafica

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,18 @@
 <template>
  <v-container fluid>
+     <v-form>
+         <v-file-input 
+         label="Selecione as Legendas"
+         prepend-icon="mdi-message-text"
+         append-outer-icon="mdi-send"
+         outlined
+         multiple 
+         chips 
+         v-model="files"
+         @click:append-outer="processSubtitles" />
+
+    
+     </v-form>
      <div class="pills">
          <Pill v-for="word in groupedWords" :key="word.name"
             :name="word.name" :amount="word.amount"/>
@@ -15,23 +28,18 @@ export default {
      components: { Pill } ,
      data: function () {
          return {
+             files: [],
              groupedWords: [
                  { name: 'i', amount: 1234 },
                  { name: 'you', amount: 900 },
                  { name: 'he', amount: 853 },                 
-                 { name: 'i', amount: 1234 },
-                 { name: 'you', amount: 900 },
-                 { name: 'he', amount: 853 },                 
-                 { name: 'i', amount: 1234 },
-                 { name: 'you', amount: 900 },
-                 { name: 'he', amount: 853 },                 
-                 { name: 'i', amount: 1234 },
-                 { name: 'you', amount: 900 },
-                 { name: 'he', amount: 853 },                 
-                 { name: 'i', amount: 1234 },
-                 { name: 'you', amount: 900 },
-                 { name: 'he', amount: 853 },                 
+                                 
              ]
+         }
+     },
+     methods: {
+         processSubtitles() {
+             console.log(this.files)
          }
      }
 }
