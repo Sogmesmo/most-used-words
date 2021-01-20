970897ff136ee07bb221f2f5d7615027bfce5a04
Geice Gomes <sogmesmo@gmail.com>
2021-01-19 22:59:43 -0300

    Utilizando electron IPC

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -21,7 +21,7 @@
 </template>
 
 <script>
-
+import {ipcRenderer} from  'electron'
 import Pill from './Pill.vue'
 
 export default {
@@ -29,17 +29,16 @@ export default {
      data: function () {
          return {
              files: [],
-             groupedWords: [
-                 { name: 'i', amount: 1234 },
-                 { name: 'you', amount: 900 },
-                 { name: 'he', amount: 853 },                 
-                                 
-             ]
+             groupedWords: []
          }
      },
      methods: {
          processSubtitles() {
-             console.log(this.files)
+              const paths = this.files.map(f => f.path)
+             ipcRenderer.send('process-subtitles' , paths)
+             ipcRenderer.on('process-subtitles', (event, resp) => {
+                this.groupedWords = resp
+             })
          }
      }
 }
